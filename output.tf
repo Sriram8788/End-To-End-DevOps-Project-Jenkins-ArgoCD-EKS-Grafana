040e964f8ed2e9a0939657e245c444035cf00372
Sriram8788 <54744038+Sriram8788@users.noreply.github.com>
2024-09-18 12:41:23 +0530

    manually uploaded

---
+++ output.tf
@@ -0,0 +1,54 @@
+output "ec2_id" {
+  value = aws_instance.jenkins_server.id
+}
+
+output "eks_cluster_id" {
+  description = "The EKS cluster ID"
+  value       = aws_eks_cluster.eks_cluster.id
+}
+
+output "eks_cluster_endpoint" {
+  description = "The endpoint for the EKS cluster"
+  value       = aws_eks_cluster.eks_cluster.endpoint
+}
+
+output "eks_cluster_arn" {
+  description = "The ARN of the EKS cluster"
+  value       = aws_eks_cluster.eks_cluster.arn
+}
+
+output "eks_cluster_security_group_id" {
+  description = "The security group ID of the EKS cluster"
+  value       = aws_eks_cluster.eks_cluster.vpc_config[0].cluster_security_group_id
+}
+
+output "eks_node_group_id" {
+  description = "The ID of the EKS node group"
+  value       = aws_eks_node_group.eks_node_group.id
+}
+
+output "alarm_name" {
+  value       = aws_cloudwatch_metric_alarm.log_metric_alarm.alarm_name
+}
+
+output "sns_topic_arn" {
+  value       = aws_sns_topic.sns_topic.arn
+}
+
+# Output for CloudWatch log group name
+output "log_group_name" {
+  description = "The name of the CloudWatch Log Group"
+  value       = aws_cloudwatch_log_group.log_group.name
+}
+
+# Output for CloudWatch log stream name
+output "log_stream_name" {
+  description = "The name of the CloudWatch Log Stream"
+  value       = aws_cloudwatch_log_stream.log_stream.name
+}
+
+# Output for VPC ID
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.my_vpc.vpc_id
+}
--- variable.tf
+++ variable.tf
@@ -1,23 +1,116 @@
 variable "region" {
-  default = "us-east-1"
+  type = string
 }
+
 variable "vpc_name" {
-    default = "VIP_vpc"
+  type = string
 }
 
 variable "cidr" {
-  default = "10.0.0.0/16"
+  type = string
 }
 
-variable "avilability_zone" {
-    type = list(string)
-    default = [ "eu-west-1a", "eu-west-1b", "eu-west-1c" ]
+variable "availability_zones" {
+  type = list(string)
 }
+
 variable "public_subnets" {
   type = list(string)
-  default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
-variable "" {
+
+variable "ami_name" {
+  type = string
+}
+
+variable "log_group_name" {
+  type = string
+}
+
+variable "retention_in_days" {
+  type = number
+}
+variable "stream_name" {
+  type = string
+}
+variable "alarm_name" {
+  type = string
+}
+variable "alarm_comparison_operator" {
+  type = string
+}
+variable "alarm_evaluation_periods" {
+  type = number
+}
+variable "alarm_threshold" {
+  type = number
+}
+variable "alarm_period" {
+  type = number
+}
+variable "alarm_namespace" {
+  type = string
+}
+variable "filter_pattern" {
+  type = string
+}
+variable "alarm_metric_name" {
+  type = string
+}
+variable "alarm_statistic" {
+  type = string
+}
+variable "sns_topic_name" {
+  type = string
+}
+variable "sns_email" {
+  type = string
+}
+
+variable "instance_id" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+/*
+variable "access_key" {
+  type      = string
+}
+
+variable "secret_key" {
+  type      = string
   
+}
+*/
+
+variable "cluster_name" {
+  description = "The name of the EKS cluster"
+  type        = string
+}
+
+variable "desired_capacity" {
+  description = "Desired number of worker nodes"
+  type        = number
+}
+
+variable "max_size" {
+  description = "Maximum number of worker nodes"
+  type        = number
+}
+
+variable "min_size" {
+  description = "Minimum number of worker nodes"
+  type        = number
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for worker nodes"
+  type        = string
+}
+
+variable "ssh_key_name" {
+  description = "SSH key name to use for EC2 instances"
+  type        = string
 }
 
