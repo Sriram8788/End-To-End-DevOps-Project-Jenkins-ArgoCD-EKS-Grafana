418fbd146b32114cc4b1211a3edd011d0c7e5d2a
Sriram8788 <54744038+Sriram8788@users.noreply.github.com>
2024-09-22 19:16:26 +0530

    update

---
+++ variable.tf
@@ -0,0 +1,118 @@
+#variable.tf
+variable "region" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "cidr" {
+  type = string
+}
+
+variable "availability_zones" {
+  type = list(string)
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "ami_name" {
+  type = string
+}
+
+variable "log_group_name" {
+  type = string
+}
+
+variable "retention_in_days" {
+  type = number
+}
+variable "stream_name" {
+  type = string
+}
+variable "alarm_name" {
+  type = string
+}
+variable "alarm_comparison_operator" {
+  type = string
+}
+variable "alarm_evaluation_periods" {
+  type = number
+}
+variable "alarm_threshold" {
+  type = number
+}
+variable "alarm_period" {
+  type = number
+}
+variable "alarm_namespace" {
+  type = string
+}
+variable "filter_pattern" {
+  type = string
+}
+variable "alarm_metric_name" {
+  type = string
+}
+variable "alarm_statistic" {
+  type = string
+}
+variable "sns_topic_name" {
+  type = string
+}
+variable "sns_email" {
+  type = string
+}
+
+variable "instance_id" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+/*
+variable "access_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "secret_key" {
+  type      = string
+  sensitive = true
+}
+*/
+
+variable "cluster_name" {
+  description = "The name of the EKS cluster"
+  type        = string
+}
+
+variable "desired_capacity" {
+  description = "Desired number of worker nodes"
+  type        = number
+}
+
+variable "max_size" {
+  description = "Maximum number of worker nodes"
+  type        = number
+}
+
+variable "min_size" {
+  description = "Minimum number of worker nodes"
+  type        = number
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for worker nodes"
+  type        = string
+}
+
+variable "ssh_key_name" {
+  description = "SSH key name to use for EC2 instances"
+  type        = string
+}
+
