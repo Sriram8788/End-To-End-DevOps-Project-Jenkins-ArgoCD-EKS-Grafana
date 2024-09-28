332e1d498c6489e596f59bb4faacf978939917dc
Sriram8788 <54744038+Sriram8788@users.noreply.github.com>
2024-09-28 23:52:43 +0530

    Update sshd.sh

--- sshd.sh
+++ sshd.sh
@@ -3,6 +3,8 @@ sudo yum install ansible -y
 sudo yum install python3-pip -y
 pip3 install boto3 botocore
 sudo yum install git -y
+ansible-galaxy collection install community.kubernetes
+
 git clone https://github.com/Sriram8788/End-To-End-DevOps-Project-Jenkins-ArgoCD-EKS-Grafana.git
 
 mv End-To-End-DevOps-Project-Jenkins-ArgoCD-EKS-Grafana Devops_project
